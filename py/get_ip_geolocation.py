ca958e6a5a6a8fc7b766d12acfbe38c82fde8f5d
Luis Gerardo Baeza <lgbaezac@gmail.com>
2024-06-19 11:52:54 -0400

    Create get_ip_geolocation.py

---
+++ py/get_ip_geolocation.py
@@ -0,0 +1,12 @@
+def get_geolocation(ip_address):
+    request_url = 'https://geolocation-db.com/jsonp/' + ip_address
+    response = requests.get(request_url).content.decode()
+    result = response.split("(")[1].strip(")")
+    result  = json.loads(result)
+    
+    country_name = result["country_name"]
+    state = result["state"]
+    latitude = result["latitude"]
+    longitude = result["longitude"]
+
+    return country_name, state, latitude, longitude
